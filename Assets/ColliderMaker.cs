9a1c69ef023c58910d0d2257cd2eec6208ea9549
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-02-12 14:06:47 -0800

    Added resources and mesh colliders

---
+++ Assets/ColliderMaker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderMaker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(Transform child in this.GetComponentInChildren<Transform>()) {
+            MeshCollider collider = child.gameObject.AddComponent<MeshCollider>();
+            collider.sharedMesh = child.gameObject.GetComponent<MeshFilter>().mesh;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
