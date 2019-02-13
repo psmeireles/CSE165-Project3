de5be0013c795bf24cfc95bf24410edd94a096f3
rdhub <rd.richard.du@gmail.com>
2019-02-12 17:48:23 -0800

    Updated campus/sphere scale and initial camera position/orientation

--- Assets/TrackController.cs
+++ Assets/TrackController.cs
@@ -8,6 +8,7 @@ public class TrackController : MonoBehaviour
     public TextAsset trackFile;
     public GameObject checkPoint;
     public GameObject campus;
+    public GameObject playerRig;
 
     List<GameObject> checkpoints;
     // Start is called before the first frame update
@@ -29,10 +30,14 @@ public class TrackController : MonoBehaviour
             float z = float.Parse(items[2]);
 
             GameObject obj = GameObject.Instantiate(checkPoint);
-            Vector3 translation = new Vector3(x, y, z);
-            obj.transform.Translate(translation + campus.transform.position);
+            Vector3 translation = new Vector3(x, y, z) * 0.0254f;
+            obj.transform.Translate(translation);
+            
             checkpoints.Add(obj);
         }
+
+        playerRig.transform.position = checkpoints[0].transform.position;
+        playerRig.transform.LookAt(checkpoints[1].transform.position);
     }
 
     // Update is called once per frame
