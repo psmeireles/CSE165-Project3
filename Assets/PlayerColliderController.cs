03acc4f2acd141833b03c9abc133f72025b6e485
Pedro Sousa Meireles <psmeireles25@gmail.com>
2019-02-21 15:51:19 -0800

    Added explosion sound

--- Assets/PlayerColliderController.cs
+++ Assets/PlayerColliderController.cs
@@ -6,6 +6,9 @@ public class PlayerColliderController : MonoBehaviour
 {
 
     public static bool hasCollided;
+    public AudioClip explosion;
+
+    private AudioSource explosionSound;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +24,7 @@ public class PlayerColliderController : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         hasCollided = true;
+        AudioSource audioSource = this.GetComponent<AudioSource>();
+        audioSource.PlayOneShot(explosion, 2);
     }
 }
